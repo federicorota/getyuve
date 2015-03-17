5f4092e0bc6394d50f124a20f744b1d0c9d9e066
noit01 <born2b@live.it>
2015-03-17 08:43:26 +0100

    mounted spree on /store

--- config/routes.rb
+++ config/routes.rb
@@ -16,17 +16,18 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
+
   # This line mounts Spree's routes at the root of your application.
   # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
-  mount Spree::Core::Engine, :at => '/'
+  mount Spree::Core::Engine, :at => '/store'
           # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
