9c1c719113b9936985132491285c3c9f3f7d5bf0
viperouge <viperouge@gmail.com>
2015-03-23 22:03:13 -0400

    restored attachemnt_file cosi heroku non whina

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,17 +1,11 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
-  has_attached_file :data,
-                    :url => ":s3_domain_url",
-                    :path => "/ckeditor/:id/:basename_:style.:extension",
+  has_attached_file :data
 
-                    :s3_credentials => {
-      					access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      					secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      					bucket: ENV['S3_BUCKET_NAME']
-      				},
+  Ckeditor::AttachmentFile.attachment_definitions[:data][:path] = '/:class/:id/:style/:basename.:extension'
+  Ckeditor::AttachmentFile.attachment_definitions[:data][:url] = '/:class/:id/:style/:basename.:extension'
 
+  validates_attachment_size :data, less_than: 100.megabytes
   validates_attachment_presence :data
-  validates_attachment_size :data, :less_than => 100.megabytes
-  do_not_validate_attachment_file_type :data
 
   def url_thumb
     @url_thumb ||= Ckeditor::Utils.filethumb(filename)
