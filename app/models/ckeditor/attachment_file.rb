9455ec6cfea2405d518bd81e7f1a5ab58eed6303
viperouge <viperouge@gmail.com>
2015-03-23 21:22:30 -0400

    polpi piu polpi uguale googlepolpi, fixato tutto il blog ariamo

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,7 +1,13 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
   has_attached_file :data,
-                    :url => "/ckeditor_assets/attachments/:id/:filename",
-                    :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
+                    :url => ":s3_domain_url",
+                    :path => "/ckeditor/:id/:basename_:style.:extension",
+
+                    :s3_credentials => {
+      					access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      					secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      					bucket: ENV['S3_BUCKET_NAME']
+      				},
 
   validates_attachment_presence :data
   validates_attachment_size :data, :less_than => 100.megabytes
